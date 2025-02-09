1e2f2c03bef6c825d87ad279bb0d5f4d8fdba484
Marius Gerdes <integr@gmail.com>
2025-02-09 22:36:13 +0100

    Add admin panel link to footer.

--- src/footer.php
+++ src/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy; <?php echo date('Y') . " " . WEBSITE_NAME; ?> All rights reserved. Powered by <a href='https://github.com/mglambda/get-off-my-lawn'>get-off-my-lawn</a></p>
+    <p><small>&copy; <?php echo date('Y') . " " . WEBSITE_NAME; ?> All rights reserved. Powered by <a href='https://github.com/mglambda/get-off-my-lawn'>get-off-my-lawn</a>.<a href="/admin.php">Admin</a>.</small></p>
 </footer>
 </body>
 </html>
