c9e978fdebdfac4e86fa4412e5f84e9826274d5e
Marius Gerdes <integr@gmail.com>
2025-02-11 13:48:04 +0100

    php-cs-fixer

--- src/gomllib.php
+++ src/gomllib.php
@@ -1,8 +1,9 @@
 <?php
 
 
-function get_post_url_relative($post_title) {
-return '/p/' . str_replace(' ', '-', $post_title);
+function get_post_url_relative($post_title)
+{
+    return '/p/' . str_replace(' ', '-', $post_title);
 }
 
 function display_post_row($conn, $row)
@@ -86,64 +87,68 @@ function display_post_by_title($conn, $title)
 }
 
 
-function _change_verbosity($verbosity, $direction=1) {
-// direction 1 for up, -1 for down
-$values = [0, 10, 100];
-// find index for given verbosity
-for($i=0; $i < count($values); $i++) {
-$last_index = $i;
-if($values[$i] >= $verbosity) {
-break;
-}
+function _change_verbosity($verbosity, $direction = 1)
+{
+    // direction 1 for up, -1 for down
+    $values = [0, 10, 100];
+    // find index for given verbosity
+    for ($i = 0; $i < count($values); $i++) {
+        $last_index = $i;
+        if ($values[$i] >= $verbosity) {
+            break;
+        }
+    }
+
+    return $values[max(0, ($last_index + $direction) % count($values))];
 }
 
-	return $values[max(0, ($last_index + $direction) % count($values))];
+function increase_verbosity($verbosity)
+{
+    return _change_verbosity($verbosity, 1);
 }
 
-function increase_verbosity($verbosity) {
-return _change_verbosity($verbosity, 1);
+function decrease_verbosity($verbosity)
+{
+    return _change_verbosity($verbosity, -1);
 }
 
-function decrease_verbosity($verbosity) {
-return _change_verbosity($verbosity, -1);
-}
-
-function display_posts($conn, $verbosity=10) {
-// gives a view of all unhidden posts in various forms
-// verbosity = 0 means only post titles
-// verbosity = 10 means title and abbreviated content, the default
-// verbosity >= 100 means full post
-$sql = "SELECT * FROM `" . TABLE_PREFIX . "posts` WHERE hidden = 0 ORDER BY created_at DESC";
-$result = $conn->query($sql);
+function display_posts($conn, $verbosity = 10)
+{
+    // gives a view of all unhidden posts in various forms
+    // verbosity = 0 means only post titles
+    // verbosity = 10 means title and abbreviated content, the default
+    // verbosity >= 100 means full post
+    $sql = "SELECT * FROM `" . TABLE_PREFIX . "posts` WHERE hidden = 0 ORDER BY created_at DESC";
+    $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-	if($verbosity==0) {
-	echo '<ul>';
-	}
-	
+        if ($verbosity == 0) {
+            echo '<ul>';
+        }
+
         while ($row = $result->fetch_assoc()) {
-		switch($verbosity) {
-		case 0:
-echo '<li><a href="' . get_post_url_relative($row['title']) . '">' . $row['title'] . '</a></li>';
-break;
-		case 10:
-            display_post_row_short($conn, $row);
-			break;
-			case 100:
-			// this is a full view
-display_post_by_title($conn, $row['title']);
-echo '<br>';
-break;
-			} // end switch
-			if($verbosity == 0) {
-			echo '</ul>';
-			}
+            switch ($verbosity) {
+                case 0:
+                    echo '<li><a href="' . get_post_url_relative($row['title']) . '">' . $row['title'] . '</a></li>';
+                    break;
+                case 10:
+                    display_post_row_short($conn, $row);
+                    break;
+                case 100:
+                    // this is a full view
+                    display_post_by_title($conn, $row['title']);
+                    echo '<br>';
+                    break;
+            } // end switch
+            if ($verbosity == 0) {
+                echo '</ul>';
+            }
 
         }
     } else {
         echo '<p>No posts found.</p>';
     }
-            echo '<br>';	
+    echo '<br>';
 }
 
 ?>
--- src/view_post.php
+++ src/view_post.php
@@ -12,21 +12,21 @@ if (isset($_GET['post'])) {
     display_post_by_title($conn, $title);
     echo '</main>';
 } else {
-$page_title = 'Posts';
+    $page_title = 'Posts';
     include 'header.php';
-	if(isset($_GET['verbosity'])) {
-	$verbosity = $_GET['verbosity'];
-	}else {
-	$verbosity=0;
-	}
+    if (isset($_GET['verbosity'])) {
+        $verbosity = $_GET['verbosity'];
+    } else {
+        $verbosity = 0;
+    }
 
-echo '<p>Showing all Posts.</p>';
-echo '<a href="?verbosity=' . increase_verbosity($verbosity) . '">Expand Posts</a>';
-echo '<br>';
-echo '<a href="?verbosity=' . decrease_verbosity($verbosity) . '">Collapse Posts</a>';
-echo '<br>';
+    echo '<p>Showing all Posts.</p>';
+    echo '<a href="?verbosity=' . increase_verbosity($verbosity) . '">Expand Posts</a>';
+    echo '<br>';
+    echo '<a href="?verbosity=' . decrease_verbosity($verbosity) . '">Collapse Posts</a>';
+    echo '<br>';
 
-display_posts($conn, $verbosity);
+    display_posts($conn, $verbosity);
 }
 
 include 'footer.php';
