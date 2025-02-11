95b6f8e04bda872c4c20f776f97392d60b0f43a0
Marius Gerdes <integr@gmail.com>
2025-02-11 23:59:36 +0100

    php-cs-fixer (still working on hook).

--- src/globals.php
+++ src/globals.php
@@ -13,22 +13,22 @@ if (!defined('WEBSITE_NAME')) {
     /* General Settings */
 
     // The Website name, displayed in page titles and in the RSS feed.
-define('WEBSITE_NAME', 'My Blog');
+    define('WEBSITE_NAME', 'My Blog');
 
     // Wether the website name is displayed at the top of the page at all.
-define('WEBSITE_NAME_SHOW', 'on');
+    define('WEBSITE_NAME_SHOW', 'on');
 
     // Wether the website name is displayed over the banner image.
-define('WEBSITE_NAME_OVERLAY_BANNER', 'on');
+    define('WEBSITE_NAME_OVERLAY_BANNER', 'on');
 
     // If true, yourdomain.com/rss/ generates an RSS feed (atom format), which is advertised in the website header.
-define('RSS_PUBLISH_ENABLED', 'on');
+    define('RSS_PUBLISH_ENABLED', 'on');
 
     // How many posts should be displayed per page before starting to paginate. Set to 0 or lower to disable pagination entirely.
-define('PAGINATION_POSTS_PER_PAGE', 20);
+    define('PAGINATION_POSTS_PER_PAGE', 20);
 
     // Should setup.php prefill the database with some example content? Set to false if you don't want Example post, message of the day etc. to be included in the website when you run setup.php. This also won't happen if your database already contains entries for the relevant tables.
-define('SETUP_CREATE_EXAMPLES', 'on');
+    define('SETUP_CREATE_EXAMPLES', 'on');
 
     /* End of General Settings */
 
